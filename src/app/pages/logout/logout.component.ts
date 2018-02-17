2f0dc04ea2d3e84cdc5359f9f98fa97bca2f124b
Aaron Osher <aaron@oshers.com>
2018-02-17 17:34:47 +0000

    Update logoutComponent
    Add Logout functionality

--- src/app/pages/logout/logout.component.ts
+++ src/app/pages/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
 
   ngOnInit() {
+    this.logout();
+    this.router.navigateByUrl('/');
   }
 
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
