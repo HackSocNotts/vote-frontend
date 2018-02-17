27e2a2f541d3f11f5e71b47d9e5be72644afcf8a
Aaron Osher <aaron@oshers.com>
2018-02-17 22:19:34 +0000

    UpdateCreateComponent
    Add validation and navigation

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -7,9 +7,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
+  progress: number;
+
+  electionName: string;
+
+  description: string;
+
+  electionCode: string;
+
+  error: string;
+
+  showAdd = true;
+  showDescription = false;
+  showCode = false;
+  showReview = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.progress = 33;
   }
 
+  canContinue() {
+    if (this.error === '') {
+      return true;
+    }
+    return false;
+  }
+
+  toAdd() {
+    if (this.canContinue()) {
+      this.progress = 33;
+      this.showAdd = true;
+      this.showDescription = false;
+      this.showCode = false;
+      this.showReview = false;
+    }
+  }
+
+  toDescription() {
+    if (this.canContinue()) {
+      this.progress = 50;
+      this.showAdd = false;
+      this.showDescription = true;
+      this.showCode = false;
+      this.showReview = false;
+    }
+  }
+
+  toCode() {
+    if (this.canContinue()) {
+      this.progress = 68;
+      this.showAdd = false;
+      this.showDescription = false;
+      this.showCode = true;
+      this.showReview = false;
+    }
+  }
+
+  toReview() {
+    if (this.canContinue()) {
+      this.progress = 80;
+      this.showAdd = false;
+      this.showDescription = false;
+      this.showCode = false;
+      this.showReview = true;
+    }
+  }
+
+  checkName() {
+    if (this.electionName.length < 3) {
+      this.error = 'Please enter an election name.';
+    } else {
+      this.error = '';
+    }
+  }
+
+  checkDescription() {
+    if (this.description.length < 3) {
+      this.error = 'Please enter a valid description.';
+    } else {
+      this.error = '';
+    }
+  }
+
+  checkCode() {
+  //  Firebase check code
+    if (this.electionCode.length !== 5) {
+      this.error = 'ID must be 5 characters long';
+    } else if (!this.isAlphaNumeric(this.electionCode)) {
+      this.error = 'ID must be alphanumeric';
+    } else {
+      this.error = '';
+    }
+  }
+
+  isAlphaNumeric(ch) {
+    return ch.match(/^[a-z0-9]+$/i) !== null;
+  }
 }
