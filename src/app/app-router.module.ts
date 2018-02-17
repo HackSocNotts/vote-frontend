342f721ec803ab840672d037d5aa0d87cc00f160
Aaron Osher <aaron@oshers.com>
2018-02-17 17:21:44 +0000

    Upda Router
    Add Login and Logout

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { LogoutComponent } from './pages/logout/logout.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent }
+];
 
 @NgModule({
   imports: [
