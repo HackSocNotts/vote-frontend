dfc17631af6b4732cac8d63901cabef9c2cee87e
Aaron Osher <aaron@oshers.com>
2018-02-18 05:43:04 +0000

    Update Ballot Component
    Add Ballots Retrieval

--- src/app/pages/ballot/ballot.component.ts
+++ src/app/pages/ballot/ballot.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ElectionService} from '../../services/election.service';
 import {LocalStorageService} from 'angular-2-local-storage';
+import {BallotService} from '../../services/ballot.service';
 
 @Component({
   selector: 'app-ballot',
@@ -13,13 +14,30 @@ export class BallotComponent implements OnInit {
 
   constructor(
     private electionService: ElectionService,
-    private localStorage: LocalStorageService
+    private localStorage: LocalStorageService,
+    private balltService: BallotService
   ) { }
 
   ngOnInit() {
-    this.electionService.getDocument(this.localStorage.get('election'))
+    const election_id: string = this.localStorage.get('election');
+    this.electionService.getDocument(election_id)
       .subscribe(data => {
         this.election = data;
+      });
+    this.balltService.getBallots(election_id)
+      .subscribe(data => {
+        const ballots: any[] = [];
+        for (let i = 0; i < data.length; i++) {
+          const ballot = data[i];
+          ballots[i] = {
+            id: ballot.payload.doc.id,
+            name: ballot.payload.doc.data().name,
+            description: ballot.payload.doc.data().description,
+            type: ballot.payload.doc.data().type,
+            options: ballot.payload.doc.data().options,
+          };
+        }
+        this.election.ballots = ballots;
         console.log(this.election);
       });
   }
