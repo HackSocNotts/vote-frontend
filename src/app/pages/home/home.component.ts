84bb206c31a99222196e183c8a05833d0b1e0e7b
Aaron Osher <aaron@oshers.com>
2018-02-18 04:15:38 +0000

    Update HomeComponent
    Add Election Check

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ElectionService} from '../../services/election.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  code: string[14];
+
+  election: string[5];
+
+  elector: string[8];
+
+  error; string;
+
+  electionObject: any;
+
+  constructor(
+    private electionService: ElectionService
+  ) { }
 
   ngOnInit() {
+  }
+
+  showInvalidCodeError() {
+    this.error = 'Invalid Code';
+    console.log(this.error);
+  }
+
+  checkCode() {
+    this.error = '';
+    if (this.code.length >= 5) {
+      const split = this.code.split('-');
+      if (split.length > 2) {
+        this.showInvalidCodeError();
+      }
+
+      if (split[0] !== this.election) {
+        this.election = split[0];
+        this.electionService.getDocument(this.election)
+          .subscribe(data => {
+            if (data === null) {
+              this.showInvalidCodeError();
+              this.electionObject = null;
+            } else {
+              this.electionObject = data;
+            }
+          });
+      }
+    }
   }
 
 }
