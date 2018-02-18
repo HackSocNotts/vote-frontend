e57d7095024b6431f490f54e9b756f9b239b52d4
Aaron Osher <aaron@oshers.com>
2018-02-18 11:12:21 +0000

    Update ElectorateService
    Add code retrieval

--- src/app/services/electorate.service.ts
+++ src/app/services/electorate.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+
 
 @Injectable()
 export class ElectorateService {
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { }
 
+  getElectorate(election: string) {
+    const ballots = this.afs.collection('election/' + election + '/electorate', refs => {return refs;});
+    return ballots.snapshotChanges();
+  }
 }
