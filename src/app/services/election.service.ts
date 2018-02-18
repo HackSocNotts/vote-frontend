1e949bdc2f5a023b2627d54be03b921f26abcf3a
Aaron Osher <aaron@oshers.com>
2018-02-18 03:03:59 +0000

    Update electionService
    Add election implemented

--- src/app/services/election.service.ts
+++ src/app/services/election.service.ts
@@ -14,15 +14,39 @@ export class ElectionService {
   /**
    * Finds document for specific user id
    * @param {string} uid
+   * @return {Observable<any>}
    */
   searchForUser(uid: string) {
     const election = this.afs.collection('election', ref => ref.where('user', '==', uid));
     return election.snapshotChanges();
   }
 
+  /**
+   * Fethces a specific document(election)
+   * @param {string} uid
+   * @return {Observable<any>}
+   */
   getDocument(uid: string) {
     const document = this.afs.doc('election/' + uid);
     return document.valueChanges();
+  }
+
+  /**
+   * Adds a new election
+   * @param {string} uid
+   * @param {string} name
+   * @param {string} description
+   * @param {string} user
+   * @return {Promise<void>}
+   */
+  addElection(uid: string, name: string, description: string, user: string) {
+    const collection = this.afs.collection('election');
+    const doc = collection.doc(uid);
+    return doc.set({
+      name: name,
+      description: description,
+      user: user
+    });
   }
 
 }
