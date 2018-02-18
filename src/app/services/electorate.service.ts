e3d33d512ee1535f89f0c73434a7f3d7c0c706ec
Aaron Osher <aaron@oshers.com>
2018-02-18 11:28:13 +0000

    Update ElectorateService
    Generate Codes Working

--- src/app/services/electorate.service.ts
+++ src/app/services/electorate.service.ts
@@ -13,4 +13,21 @@ export class ElectorateService {
     const ballots = this.afs.collection('election/' + election + '/electorate', refs => {return refs;});
     return ballots.snapshotChanges();
   }
+
+  generate(count: number, uid: string) {
+    for (let i = 0; i < count; i++) {
+      this.addCode(this.generateCode(), uid);
+    }
+  }
+
+  addCode(code: string[8], uid: string) {
+    const collection = this.afs.collection('election/' + uid + '/electorate');
+    const doc = collection.doc(code);
+    console.log('new code: %s', code);
+    return doc.set({});
+  }
+
+  generateCode() {
+    return Math.random().toString(36).substring(2, 6) + Math.random().toString(36).substring(8, 12);
+  }
 }
