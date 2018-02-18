0a29c6ec6457bb420b7ef654b811f67b60015d16
Aaron Osher <aaron@oshers.com>
2018-02-18 04:34:54 +0000

    Update HomeComponent
    Validation Complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,9 +14,15 @@ export class HomeComponent implements OnInit {
 
   elector: string[8];
 
-  error; string;
+  error: string;
+
+  errorMessage: string;
 
   electionObject: any;
+
+  electorObject: any;
+
+  validCode = false;
 
   constructor(
     private electionService: ElectionService
@@ -25,17 +31,24 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  showInvalidCodeError() {
+  showInvalidCodeError(message: string) {
     this.error = 'Invalid Code';
+    this.errorMessage = message;
+    this.validCode = false;
     console.log(this.error);
   }
 
   checkCode() {
     this.error = '';
+    this.validCode = true;
+    if (this.code.length < 14) {
+      this.validCode = false;
+    }
+
     if (this.code.length >= 5) {
       const split = this.code.split('-');
       if (split.length > 2) {
-        this.showInvalidCodeError();
+        this.showInvalidCodeError('Code has too many parts.');
       }
 
       if (split[0] !== this.election) {
@@ -43,13 +56,34 @@ export class HomeComponent implements OnInit {
         this.electionService.getDocument(this.election)
           .subscribe(data => {
             if (data === null) {
-              this.showInvalidCodeError();
+              this.showInvalidCodeError('No election exists for this code.');
               this.electionObject = null;
             } else {
               this.electionObject = data;
             }
           });
       }
+    }
+
+    if (this.code.length === 14) {
+      const split = this.code.split('-');
+      if (split[1] !== this.elector) {
+        this.elector = split[1];
+        this.electionService.getDocument(this.election + '/electorate/' + this.elector)
+          .subscribe(data => {
+            if (data === null) {
+              this.showInvalidCodeError('Code does not exist.');
+              this.electorObject = null;
+            } else {
+              this.electorObject = data;
+              this.validCode = true;
+            }
+          });
+      }
+    }
+
+    if (this.code.length > 14) {
+      this.showInvalidCodeError('Code is too long');
     }
   }
 
