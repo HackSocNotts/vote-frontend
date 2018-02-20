650e3beb509d13b481beba6cc831a3b85a26a364
Aaron Osher <aaron@oshers.com>
2018-02-20 22:51:29 +0000

    Update Candidate Service
    Added Add, Remove, and Get functionality to the service

--- src/app/services/candidates.service.ts
+++ src/app/services/candidates.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 
 @Injectable()
 export class CandidatesService {
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { }
 
+  getCandidates(election: string) {
+    const ballots = this.afs.collection('election/' + election + '/candidates', refs => {return refs;});
+    return ballots.snapshotChanges();
+  }
+
+  deleteCandidate(election: string, candidate: string) {
+    const document = this.afs.doc('election/' + election + '/candidates/' + candidate);
+    return document.delete();
+  }
+
+  addCandidate(election: string, candidate: any) {
+    const collection = this.afs.collection('election/' + election + '/candidates');
+    return collection.add({
+      name: candidate.name,
+      manifesto: candidate.manifesto,
+    });
+  }
 }
