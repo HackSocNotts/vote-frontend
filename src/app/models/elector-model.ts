1c2b25790654c4e0da405f9561b0b8bbc2a3c0e0
Aaron Osher <aaron@oshers.com>
2018-02-21 22:07:55 +0000

    Update elector model
    Add votes and locked check

--- src/app/models/elector-model.ts
+++ src/app/models/elector-model.ts
@@ -3,4 +3,14 @@ export interface ElectorModel {
    * UID of electorate code
    */
   id: string;
+
+  /**
+   * Whether or not the voters vote is locked
+   */
+  locked: boolean;
+
+  /**
+   * Object container vote data
+   */
+  votes: any;
 }
