4c202b3bc0a0b892e6c036cef86c218e63d14e9e
Aaron Osher <aaron@oshers.com>
2018-02-22 16:35:48 +0000

    Update Results Component
    Add Election Retrieval

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {LocalStorageService} from 'angular-2-local-storage';
+import {ElectionService} from '../../services/election.service';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  election_id: string;
+
+  election$: Observable<any>;
+
+  constructor(
+    private localStroage: LocalStorageService,
+    private electionService: ElectionService
+  ) { }
 
   ngOnInit() {
+    this.election_id = this.localStroage.get('election');
+    this.retrieveElection();
+  }
+
+  private retrieveElection() {
+    this.election$ = this.electionService.getDocument(this.election_id)
+      .pipe(
+        map(data => {
+          return {
+            id: this.election_id,
+            name: data.name,
+            description: data.description,
+            locked: (data.locked) ? data.locked : false
+          };
+        })
+      );
   }
 
 }
