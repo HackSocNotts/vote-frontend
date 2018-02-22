72ec76e8f0af41e013dd669a2fa9f13f5389c2ef
Aaron Osher <aaron@oshers.com>
2018-02-22 17:49:29 +0000

    Update Received Votes Component
    Add Electorate Retrieval

--- src/app/pages/results/recieved-votes/recieved-votes.component.ts
+++ src/app/pages/results/recieved-votes/recieved-votes.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ElectorModel} from '../../../models/elector-model';
+import {ElectorateService} from '../../../services/electorate.service';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-recieved-votes',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceivedVotesComponent implements OnInit {
 
-  constructor() { }
+  @Input() election: {
+    id: string,
+    name: string,
+    description: string,
+    locked: boolean
+  };
+
+  electorate$: Observable<ElectorModel[]>;
+
+  constructor(
+    private electorService: ElectorateService
+  ) { }
 
   ngOnInit() {
+    this.retrieveElectorate();
+  }
+
+  private retrieveElectorate() {
+    this.electorate$ = this.electorService.getElectorate(this.election.id)
+      .pipe(
+        map(data => {
+          const formatted = [];
+          for (let i = 0; i < data.length; i++) {
+            formatted[i] = {
+              id : data[i].payload.doc.id,
+              locked: data[i].payload.doc.data().locked,
+              votes: data[i].payload.doc.data().votes
+            };
+          }
+          return formatted;
+        })
+      );
   }
 
 }
